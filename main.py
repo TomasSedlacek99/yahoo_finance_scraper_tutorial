6a84200c5983ba070e17e6932118ff5439172749
Tomas Sedlacek <tomino99@gmail.com>
2023-06-20 15:05:31 +0200

    Extracting data to CSV file

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 import time
+import csv
 
 urls = ["https://finance.yahoo.com/quote/AMZN?p=AMZN&.tsrc=fin-srch", "https://finance.yahoo.com/quote/GOOG/", "https://finance.yahoo.com/quote/AAPL?p=AAPL&.tsrc=fin-srch"]
 headers = {'User-Agent' : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"}
 
+csv_file = open("scrape.csv", "w")
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['Stock Name', 'Current Price', 'Previous Close', 'Open', 'Bid', 'Ask', 'Day`s Range', '52 Week Range', 'Volume', 'Avg. Volume', 'Market Cap',
+                     'Beta (5Y Monthly)', 'PE Ratio (TTM)', 'EPS (TTM)', 'Earnings Date', 'Forward Dividend & Yield', 'Ex-Dividend Date', '1y Target Est'])
+
 for url in urls:
+    stock = []
     html_page = requests.get(url, headers=headers)
 
     soup = BeautifulSoup(html_page.content, 'lxml')
@@ -27,17 +34,24 @@ for url in urls:
     print()
     print(stock_title)
     print(current_price)
+    stock.append(stock_title)
+    stock.append(current_price)
 
     #Left side table data extraction
     for index in range(0,8):
         heading = table_left[index].find("span").get_text()
         value = table_left[index].find_all("td")[1].get_text()
+        stock.append(value)
         print(heading + ": " + value)
 
     #Right side table data extraction
     for index in range(0,8):
         heading = table_right[index].find("span").get_text()
         value = table_right[index].find_all("td")[1].get_text()
+        stock.append(value)
         print(heading + ": " + value)
 
+    csv_writer.writerow(stock)
     time.sleep(5)
+
+csv_file.close()
